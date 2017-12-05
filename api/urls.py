b732ee402c8f77f448f2d099b57b75717bea6d3a
Noah-Dev <ark_code@protonmail.com>
2017-12-04 18:01:02 -0600

    Removed python files; switching to mean stack

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from. import views
-app_name = 'api'
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^data/$', views.data, name='data'),
-    url(r'^load_file/$', views.load_file, name='data')
-]
