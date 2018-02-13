39f2731b4a45b612df99cf6c86395502c4208409
Matthew D. Scholefield <matthew3311999@gmail.com>
2018-02-12 20:18:33 -0600

    Add basic leawood importer

--- data-normalization/normalize.py
+++ data-normalization/normalize.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+import json
+
+from openpyxl import load_workbook
+
+
+def import_leawood():
+    wb = load_workbook('data/LS_Streetlights.xlsx')
+    data = [[j.value for j in i] for i in wb['LS_Stretlights']]
+    labels = data[0]
+    rows = [
+        {labels[i]: value for i, value in enumerate(row)}
+        for row in data[1:]
+    ]
+    return rows
+
+
+def main():
+    leawood = import_leawood()
+    formats = {
+        'leawood': leawood[0]
+    }
+    # Display single examples for debugging
+    print(json.dumps(formats, indent=4))
+
+
+if __name__ == '__main__':
+    main()
