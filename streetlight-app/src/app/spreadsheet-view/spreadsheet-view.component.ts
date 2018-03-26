2978bd31936489c7b7563d3f0fbe831e66c376cf
Joel Tanzi <jtanziredux@gmail.com>
2018-03-25 21:53:25 -0500

    Refactored spreadsheet component for updated streetlight model.

--- streetlight-app/src/app/spreadsheet-view/spreadsheet-view.component.ts
+++ streetlight-app/src/app/spreadsheet-view/spreadsheet-view.component.ts
@@ -11,20 +11,20 @@ export class SpreadsheetViewComponent implements OnInit {
   // Table data source
   streetlights = [];
   cols = [
-    { field: 'id', header: 'ID', filtermatchmode: 'equals' },
-    { field: 'lat', header: 'Lat', filtermatchmode: 'equals' },
-    { field: 'lon', header: 'Long', filtermatchmode: 'equals' },
-    { field: 'street', header: 'Street', filtermatchmode: 'contains' },
-    { field: 'zip', header: 'Zip', filtermatchmode: 'equals'},
-    { field: 'nema', header: 'NEMA', filtermatchmode: 'equals' },
-    { field: 'wireless', header: 'Wi-fi', filtermatchmode: 'equals' },
-    { field: 'fixture_mfg', header: 'Fixture Mfg', filtermatchmode: 'contains' }
+    { field: 'poleId', header: 'Pole ID', filtermatchmode: 'contains' },
+    { field: 'logitude', header: 'Logitude', filtermatchmode: 'contains' },
+    { field: 'latitude', header: 'Lat', filtermatchmode: 'equals' },
+    { field: 'longitude', header: 'Long', filtermatchmode: 'equals' },
+    { field: 'lightBulbType', header: 'Bulb Type', filtermatchmode: 'contains' },
+    { field: 'wattage', header: 'Wattage', filtermatchmode: 'equals'},
+    { field: 'lumens', header: 'Lumens', filtermatchmode: 'equals' },
+    { field: 'attachedTech', header: 'Attached Tech', filtermatchmode: 'equals' },
+    { field: 'poleOwner', header: 'Pole Owner', filtermatchmode: 'contains' },
+    { field: 'dataSource', header: 'Data Source', filtermatchmode: 'contains' },
+    { field: 'fiberWifiEnabled', header: 'Wi-fi', filtermatchmode: 'contains' },
+    { field: 'lightAttributes', header: 'Attributes', filtermatchmode: 'contains' },
+    { field: 'poleType', header: 'Pole Type', filtermatchmode: 'contains' }
   ];
-
-  // Table filters
-  nemaFilter: boolean;
-  wifiFilter: boolean;
-
 
   constructor( private service: StreetlightService ) { }
 
@@ -32,7 +32,6 @@ export class SpreadsheetViewComponent implements OnInit {
     const streetlightResults = this.service.getStreetlights();
     streetlightResults.subscribe((value) => {
       this.streetlights = value;
-      console.dir(this.streetlights);
     }, (error) => {
       console.error('SpreadsheetViewComponent::ngOnInit::Error: Failed to retrieve streetlight data.');
     });
