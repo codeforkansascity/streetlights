f791c5ec1a5b3c134e444a56f0081a1d7fd8eb85
Joel Tanzi <jtanziredux@gmail.com>
2018-05-09 12:41:22 -0500

    initial buildout of landing view component

--- streetlight-app/src/app/landing/landing.component.ts
+++ streetlight-app/src/app/landing/landing.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { StreetlightService } from '../../services/streetlight.service';
 
 @Component({
-  selector: 'app-landing',
+  selector: 'app-landing-view',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  lumenOptions = [
+    { label: '', value: null },
+    { label: '1000', value: 1000 },
+    { label: '1125', value: 1125 },
+    { label: '1300', value: 1300 },
+    { label: '1425', value: 1425 }
+  ];
+
+  // Data source
+  streetlights = [];
+
+  constructor( private service: StreetlightService ) { }
 
   ngOnInit() {
+
+    // Collect Streetlight data through API call
+    const streetlightResults = this.service.getStreetlights();
+    streetlightResults.subscribe((value) => {
+      this.streetlights = value;
+      console.dir(this.streetlights);
+    }, (error) => {
+      console.error('LandingViewComponent::ngOnInit::Error: Failed to retrieve streetlight data.');
+    });
+
   }
 
 }
